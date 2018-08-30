56aec41598865653ffe411b04c7e330a82170e40
datdta95 <datdta95@gmail.com>
2018-08-30 11:25:01 +0700

    create class MoveablePoint

---
+++ src/main/java/MoveablePoint.java
@@ -0,0 +1,56 @@
+public class MoveablePoint extends Point {
+    private float xSpeed = 0.0f;
+    private float ySpeed = 0.0f;
+
+    public MoveablePoint() {
+
+    }
+
+    public MoveablePoint(float xSpeed, float ySpeed) {
+        this.xSpeed = xSpeed;
+        this.ySpeed=ySpeed;
+    }
+
+    public MoveablePoint(float x, float y, float xSpeed, float ySpeed) {
+        super(x, y);
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public float getxSpeed() {
+        return xSpeed;
+    }
+
+    public void setxSpeed(float xSpeed) {
+        this.xSpeed = xSpeed;
+    }
+
+    public float getySpeed() {
+        return ySpeed;
+    }
+
+    public void setySpeed(float ySpeed) {
+        this.ySpeed = ySpeed;
+    }
+
+    public void setSpeed(float xSpeed, float ySpeed) {
+        setxSpeed(xSpeed);
+        setySpeed(ySpeed);
+    }
+
+    public float[] getSpeed() {
+        return new float[]{xSpeed, ySpeed};
+    }
+
+    @Override
+    public String toString() {
+        return "(" + getSpeed()[0] + "," + getSpeed()[1] + ")";
+    }
+
+    public MoveablePoint move() {
+        setxSpeed(super.getX() + xSpeed);
+        setySpeed(super.getY() + ySpeed);
+        return this;
+    }
+
+}
