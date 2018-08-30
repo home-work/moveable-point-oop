8da38d9094d87fc592f0178494ba5da2c485a93f
datdta95 <datdta95@gmail.com>
2018-08-30 11:25:23 +0700

    test: PASS class MoveablePoint

--- src/test/java/MoveablePointOOPTest.java
+++ src/test/java/MoveablePointOOPTest.java
@@ -10,4 +10,13 @@ public class MoveablePointOOPTest {
         String actual = point.toString();
         assertEquals(expect, actual);
     }
+
+    @Test
+    public void testMethodMove() {
+        MoveablePoint moveablePoint = new MoveablePoint(1, 2, 3, 4);
+        MoveablePoint expect = new MoveablePoint(4, 6);
+        MoveablePoint actual = moveablePoint.move();
+        assertEquals(expect.toString(), actual.toString());
+
+    }
 }
